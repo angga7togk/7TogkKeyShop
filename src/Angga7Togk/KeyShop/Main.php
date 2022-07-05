d856cb865cb0dbbdc71fa5dcb672589316b52ed4
angga7togk <90005225+angga7togk@users.noreply.github.com>
2022-07-05 20:28:13 +0700

    Fix Player Using Namespaces

--- src/Angga7Togk/KeyShop/Main.php
+++ src/Angga7Togk/KeyShop/Main.php
@@ -39,116 +39,26 @@ class Main extends PluginBase implements Listener {
         if($data === null){
           return true;
         }
+        
+        if($data === 0){
+          return true;
+        }
     
         $money = EconomyAPI::getInstance()->myMoney($player);
-        switch ($data){
-          case 0:
-            if($money >= $this->config->get("0")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("0")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("0")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("0")["Message"]["Succes"]);
+        if($money >= $this->config->get($data)["Key"]["Price"]){
+              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get($data)["Key"]["Price"]);
+              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get($data)["Key"]["Name"] . " 1 \"" . $player->getName()) . "\"";
+              $player->sendMessage($this->config->get($data)["Message"]["Succes"]);
             } else {
-              $player->sendMessage($this->config->get("0")["Message"]["Failed"]);
+              $player->sendMessage($this->config->get($data)["Message"]["Failed"]);
             }
-          break;
-        
-          case 1:
-            if($money >= $this->config->get("1")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("1")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("1")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("1")["Message"]["Succes"]);
-            } else {
-              $player->sendMessage($this->config->get("1")["Message"]["Failed"]);
-            }
-          break;
-        
-          case 2:
-            if($money >= $this->config->get("2")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("2")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("2")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("2")["Message"]["Succes"]);
-            } else {
-              $player->sendMessage($this->config->get("2")["Message"]["Failed"]);
-            }
-          break;
-        
-          case 3:
-            if($money >= $this->config->get("3")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("3")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("3")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("3")["Message"]["Succes"]);
-            } else {
-              $player->sendMessage($this->config->get("3")["Message"]["Failed"]);
-            }
-          break;
-        
-          case 4:
-            if($money >= $this->config->get("4")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("4")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("4")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("4")["Message"]["Succes"]);
-            } else {
-              $player->sendMessage($this->config->get("4")["Message"]["Failed"]);
-            }
-          break;
-        
-          case 5:
-            if($money >= $this->config->get("5")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("5")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("5")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("5")["Message"]["Succes"]);
-            } else {
-              $player->sendMessage($this->config->get("5")["Message"]["Failed"]);
-            }
-          break;
-        
-          case 6:
-            if($money >= $this->config->get("6")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("6")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("6")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("6")["Message"]["Succes"]);
-            } else {
-              $player->sendMessage($this->config->get("6")["Message"]["Failed"]);
-            }
-          break;
-        
-          case 7:
-            if($money >= $this->config->get("7")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("7")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("7")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("7")["Message"]["Succes"]);
-            } else {
-              $player->sendMessage($this->config->get("7")["Message"]["Failed"]);
-            }
-          break;
-        
-          case 8:
-            if($money >= $this->config->get("8")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("8")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("8")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("8")["Message"]["Succes"]);
-            } else {
-              $player->sendMessage($this->config->get("8")["Message"]["Failed"]);
-            }
-          break;
-        
-          case 9:
-            if($money >= $this->config->get("9")["Key"]["Price"]){
-              EconomyAPI::getInstance()->reduceMoney($player, $this->config->get("9")["Key"]["Price"]);
-              $this->getServer()->getCommandMap()->dispatch(new ConsoleCommandsender($this->getServer(), $this->getServer()->getLanguage()), "key " . $this->config->get("9")["Key"]["Name"] . " 1 " . $player->getName());
-              $player->sendMessage($this->config->get("9")["Message"]["Succes"]);
-            } else {
-              $player->sendMessage($this->config->get("9")["Message"]["Failed"]);
-            }
-          break;
-        }
       });
       $mymoney = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->myMoney($player);
       $form->setTitle($this->config->get("Title"));
       $form->setContent("§g>> §eHi, §b" . $player->getName() . "\n§g>> §eYour Balance §a" . $mymoney);
-      for($i = 0;$i <= 9;$i++){
+      for($i = 1;$i <= 100;$i++){
           if($this->config->exists($i)){
-              $form->addButton($this->config->get($i)["Button"]["Name"] . "\n§r" . $this->config->get($i)["Button"]["Sub-Name"]);
+              $form->addButton($this->config->get($i)["Button"]["Name"] . "\n§rPrice : " . $this->config->get($i)["Key"]["Price"]);
           }
       }
       $form->sendToPlayer($player);
